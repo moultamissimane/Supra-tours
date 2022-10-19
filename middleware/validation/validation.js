916554143ed47ca554991cae70c2543b33e3b4de
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-10-19 13:59:03 +0100

    add validation in middleware

---
+++ middleware/validation/validation.js
@@ -0,0 +1,36 @@
+const Joi = require('joi')
+
+function validateObj(obj, schema) {
+    let result = null
+    Joi.validate(obj, schema, (err, data) => {
+        if (err) {
+            result = [false, err]
+        }
+        else {
+            result = [true, data]
+        }
+    })
+    return result
+}
+
+function userValidation(user) {
+    const userSchema = Joi.object().keys({
+        username: Joi.string().trim().min(5).max(100).required(),
+        phone: Joi.string().trim().max(10).required(),
+        // email: Joi.string().trim().email().required(),
+    })
+    return validateObj(user, userSchema)
+}
+
+function openTicket(ticket) {
+    ticket.is_booked = false
+    ticket.save()
+        .then(data => console.log(`Opened ticket with ticketID: ${ticket._id}`))
+        .catch(err => console.log(`Failed to open ticket with ticketID: ${ticket._id}`))
+}
+
+module.exports = {
+    userValidation: userValidation,
+    openTicket: openTicket,
+}
+
